ab75300398c5afe96096f94d424dd9ec573431b7
Sharmina Akhtar <sakht318@gmail.com>
2018-03-06 14:59:19 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -14,5 +14,13 @@ class App < Sinatra::Base
   end
 
   # Code your final two routes here:
+  get '/goodbye/:name' do
+    @goodbye = Goodbye.find([params:name])
+    
+  end
 
+  get '/multiply/:num1/:num2' do
+
+  end
+  
 end
